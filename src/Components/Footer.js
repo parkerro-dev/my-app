fa0a5a68403fdd0a29407303c225c54bbd7775b7
Mark O'Neill <aoneillmark@gmail.com>
2024-03-19 22:40:36 +0000

    Added github to footer.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,21 @@
 import { Box, Text, Link, Icon, Image, useColorMode } from '@chakra-ui/react';
 import { FaArrowCircleUp } from 'react-icons/fa';
 
-
 const Footer = () => {
   const { colorMode } = useColorMode();
   const discordLogo = colorMode === 'light' 
     ? "/Images/Corpo_Logos/discord_black.png"
     : "/Images/Corpo_Logos/discord_white.png";
-    const instagramLogo = colorMode === 'light' 
+  const instagramLogo = colorMode === 'light' 
     ? "/Images/Corpo_Logos/instagram_black.png"
     : "/Images/Corpo_Logos/instagram_white.png";
-    const linkedinLogo = colorMode === 'light' 
+  const linkedinLogo = colorMode === 'light' 
     ? "/Images/Corpo_Logos/linkedin_black.png"
     : "/Images/Corpo_Logos/linkedin_white.png";
+  const githubLogo = colorMode === 'light' 
+    ? "/Images/Corpo_Logos/github_black.png" 
+    : "/Images/Corpo_Logos/github_white.png";
+  
   return (
     <Box mt={10} align="center" justifyContent="center" fontSize="xs">
       <Box mt={5} display="flex" justifyContent="center">
@@ -26,9 +29,14 @@ const Footer = () => {
             <Image src={discordLogo} alt="Discord Logo" w="50px" />
           </a>
         </Box>
-        <Box mr={0}>
+        <Box mr={4}> 
           <a href="https://www.linkedin.com/company/physoc-trinity-college-dublin/" target="_blank">
             <Image src={linkedinLogo} alt="LinkedIn Logo" w="50px" />
+          </a>
+        </Box>
+        <Box mr={0}>
+          <a href="https://github.com/aoneillmark/physoc-website?tab=readme-ov-file" target="_blank">
+            <Image src={githubLogo} alt="GitHub Logo" w="50px" />
           </a>
         </Box>
       </Box>
